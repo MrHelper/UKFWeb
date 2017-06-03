5072ae00090731ea18b314407b9234b1dc1e0e8d
Vu Le <levu3041988@gmail.com>
2017-06-04 01:53:55 +0700

    update

---
+++ js/ukf.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){       
+   		var scroll_start = 0;
+
+   		var offset = 50;
+
+	   $(document).scroll(function() { 
+	      scroll_start = $(this).scrollTop();
+	      if(scroll_start > offset) { 
+	      		console.log(123);		
+	          $("header .navbar-custom").css({ 'background-color': '#fff','box-shadow':'0 3px 5px 2px rgba(0,0,0,0.5)', '-webkit-box-shadow':'0 3px 5px 2px rgba(0,0,0,0.5)', '-moz-box-shadow':'0 3px 5px 2px rgba(0,0,0,0.5)','-o-box-shadow':'0 3px 5px 2px rgba(0,0,0,0.5)'});
+	       		$("header .navbar-custom a").css('color','#000');
+	       } else {
+	          $('header .navbar-custom').css({'background-color' :'transparent', 'box-shadow':'none','-webkit-box-shadow':'none'});
+	       		$("header .navbar-custom a").css('color','#fff');
+	       }
+	   });
+
+});
