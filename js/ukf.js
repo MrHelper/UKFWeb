cc093a8594e42867e9b2ead9806fe8c413f82060
Vu Le <levu3041988@gmail.com>
2017-06-04 18:40:21 +0700

    update

--- js/ukf.js
+++ js/ukf.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
 	      		console.log(123);		
 	          $("header .navbar-custom").css({ 'background-color': '#fff','box-shadow':'0 3px 5px 2px rgba(0,0,0,0.5)', '-webkit-box-shadow':'0 3px 5px 2px rgba(0,0,0,0.5)', '-moz-box-shadow':'0 3px 5px 2px rgba(0,0,0,0.5)','-o-box-shadow':'0 3px 5px 2px rgba(0,0,0,0.5)'});
 	       		$("header .navbar-custom a").css('color','#000');
+	       		$("header .navbar-custom .active a").css('color','#fff');
 	       } else {
 	          $('header .navbar-custom').css({'background-color' :'transparent', 'box-shadow':'none','-webkit-box-shadow':'none'});
 	       		$("header .navbar-custom a").css('color','#fff');
