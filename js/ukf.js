f0a487f33f2d32346f9097bec72284545a9d126a
Vu Le <levu3041988@gmail.com>
2017-06-04 21:07:59 +0700

    update

--- js/ukf.js
+++ js/ukf.js
@@ -5,8 +5,7 @@ $(document).ready(function(){
 
 	   $(document).scroll(function() { 
 	      scroll_start = $(this).scrollTop();
-	      if(scroll_start > offset) { 
-	      		console.log(123);		
+	      if(scroll_start > offset) { 		
 	          $("header .navbar-custom").css({ 'background-color': '#fff','box-shadow':'0 3px 5px 2px rgba(0,0,0,0.5)', '-webkit-box-shadow':'0 3px 5px 2px rgba(0,0,0,0.5)', '-moz-box-shadow':'0 3px 5px 2px rgba(0,0,0,0.5)','-o-box-shadow':'0 3px 5px 2px rgba(0,0,0,0.5)'});
 	       		$("header .navbar-custom a").css('color','#000');
 	       		$("header .navbar-custom .active a").css('color','#fff');
